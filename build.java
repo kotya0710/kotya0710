04c30e88bdd95bd68078210691ad56c10403b637
Rischat Ravilevich <rischatravilevich@hotmail.com>
2022-12-14 07:29:01 +0300

    first commit

---
+++ build.java
@@ -0,0 +1,49 @@
+// keywords:
+// content of directiory
+// read file to byte array
+// write text file
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.io.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.GraphicsConfiguration;
+import java.lang.Math;
+class build {
+    public static void main(String[] args)
+    throws IOException
+    {
+        System.out.println("Building...");
+        String path = "./src/";
+        String name = "_src_content";
+        String data = "var "+name+"=[\n";
+        String tail = "]\n";
+        String list = ".keys=[";
+        File directoryPath = new File(path);
+        String contents[] = directoryPath.list();
+        for(int i=0; i<contents.length; i++) {
+            System.out.println(contents[i]);
+            File file = new File(path+contents[i]);
+            byte[] fc = Files.readAllBytes(file.toPath());
+            data += "`" + new String(fc) + "`,\n";
+            tail += name + "['" + contents[i] + "']";
+            tail += "=" + name + "[" + i + "]\n";
+            list += "'" + contents[i] + "',";
+        }
+        data += tail + name + list + "]";
+        File file = new File(name+".js");
+        FileOutputStream fos = new FileOutputStream(file);
+        BufferedOutputStream bos =
+            new BufferedOutputStream(fos);
+        byte[] bytes = data.getBytes();
+        bos.write(bytes);
+        bos.close();
+        fos.close();
+        System.out.println("Good!");
+    }
+}
---
+++ index.js
@@ -0,0 +1,153 @@
+// array func <> arrow func
+// match don't return groups if /g set
+window.onload = function () {
+    document.body.innerHTML = ""
+    let text = _src_content['fibonacci.c']
+    
+    let _c_rules = c_rules()
+    let os=[]
+    let maxparses = 0x10
+    for(;;)
+    {
+        if(maxparses<+Infinity)
+            if(--maxparses<=0)
+                break
+        let o = parse(text, _c_rules)
+        if(!o)
+            break
+        os.push(o)
+        text = text.slice(o._len)
+    }
+
+    poop(
+        os.map((o,i) => `[${i}]`+JSON.stringify(o)).join('\n')
+    )
+
+    poop(text, { color: 'hotpink' })
+}
+
+function c_rules() {
+    return [
+        [
+            'typedef',
+            /^typedef\s+(.*)\s+([a-zA-Z0-9_]+)\s*;\s*/,
+            'type', 'alias'
+        ],
+        [
+            'function',
+            /^([a-zA-Z0-9_\s]+)\s+([a-zA-Z0-9_]+)\s*\((.*)\)\s*/,
+            'type', 'name', 'args'
+        ],
+        [
+            'open figure brace',
+            /^{\s*/,
+            null
+        ],
+        [
+            'подпроцедура или тип и процедура',
+            '[a-zA-Z0-9_\s]+(.*)\s*;',
+            'a'
+        ]
+    ]
+        .map(([a, b, ...x]) => typeof b == 'string' ?
+            [a, new RegExp('^' + b + '\\s*'), ...x] :
+            [a, b, ...x])
+}
+
+function parse(text, ps) {
+    let o
+    for(let p of ps)
+        if((o = parse_(text, ...p)))
+            break;
+    return o;
+}
+
+function parse_(text, _name, re, ...a) {
+    if(typeof re == 'string')
+        poop_err('re cannot be string')
+    let m = text.match(re)
+    if(!m)
+        return null
+    let o = { _name, _len: m[0].length, _str: m[0] }
+    if(m.length - 1 > a.length)
+        poop_err('You want too less arguments')
+    if(a.length == 1 && a[0] === null)
+        return o // No arguments
+    for(let i=0; i<a.length; ++i)
+        o[a[i]] = m[i+1]
+    if(!m[0].length)
+        poop_err('Cannot be 0, check re')
+    return o
+}
+
+function poop_err(t = 'poop', s = {}, ...a)
+{
+    s.color='white'
+    s.background='red'
+    poop(t, s, ...a)
+    throw Error(t)
+}
+
+function poop(text = 'poop', style = {}) {
+    console.log(text)
+    let poop = document.createElement('div')
+    poop.innerText = text
+    poop.style.color = style.color || 'black'
+    poop.style.background = style.background || 'white'
+    document.body.append(poop)
+}
+
+// Adds ._string field to the provided image
+function string_from_image(im)
+{
+    if(im._string==null)
+    {
+        let cvs=document.body.querySelector('canvas')
+        if(!cvs)
+        {
+            cvs=document.createElement('canvas')
+            document.body.append(cvs)
+        }
+        let ctx=cvs.getContext('2d')
+        cvs.width=im.width
+        cvs.height=im.height
+        ctx.drawImage(im, 0, 0)
+        im._string = ":("
+        im._string = cvs.toDataURL("image/png")
+    }
+    return im
+}
+
+function cvs_as_string(ctx, cvs) {
+    let d=ctx.getImageData(0, 0, cvs.width, cvs.height).data
+    return String.fromCodePoint(...d)
+}
+
+function _anon1() {
+    // uses string_from_image
+    let ims=[...document.body.querySelectorAll('img')]
+        .map(im => string_from_image(im))
+    let text = ims.map(e=>e._string).join('\n\n\n')
+    document.body.innerText += text
+}
+
+async function _anon2() {
+    // uses string_from_image
+    let ims=[]
+    ims.ready = Promise.all(images
+        .split('\n')
+        .filter(e => e)
+        .map(file => 'images/'+file)
+        .map(src => new Promise(r => {
+            let im=new Image()
+            im.src=src
+            im.onload = r
+            ims.push(im)
+            ims[src]=im
+            im.as_string=()=>string_from_image(im)
+        })))
+    // do something ...
+    await ims.ready
+    let text = ims.map(e=>e._string).join('\n\n\n')
+    document.body.innerText += text
+}
